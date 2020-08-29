8cca842e678c86a1774879db723debfcc1011282
rtrrsr <376477035@qq.com>
2020-08-29 15:01:22 +0800

    比较运算符

---
+++ src/Compare.java
@@ -0,0 +1,12 @@
+    //创建类
+public class Compare {
+    //主方法 权限修饰类 静态修饰类 返回值修饰类
+    public static final void main(String[] args) throws Exception {
+        int number_1 = 5;          //定义整形局部变量
+        int number_2 = 7;
+        System.out.println("number_1 > number_2的返回值" + (number_1 > number_2));
+        System.out.println("number_1 < number_2的返回值" + (number_1 < number_2));
+        System.out.println("number_1 == number_2的返回值" + (number_1 == number_2));
+        System.out.println("number_1 != number_2的返回值" + (number_1 != number_2));
+    }
+}
