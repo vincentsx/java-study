d38b080140fc36eafb7c71d0b27e552ea1a70368
rtrrsr <376477035@qq.com>
2020-08-29 10:22:51 +0800

    局部变量

---
+++ src/Val.java
@@ -0,0 +1,8 @@
+    //创建类
+public class Val {
+    static int times = 3;       //定义成员变量times
+    public static void main(String[] args){
+        int times = 6;          //定义局部变量times
+        System.out.println("times的值为：" + times);
+    }
+}
