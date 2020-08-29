489c12a59b45365c92e8780210b6c51de06dfc97
rtrrsr <376477035@qq.com>
2020-08-29 16:42:30 +0800

    简单的if条件语句

---
+++ src/Getif.java
@@ -0,0 +1,14 @@
+    //创建类 简单的if条件语句
+public class Getif{
+    //主方法 权限修饰符 静态修饰符 返回值修饰符
+    public static void main(String[] args) throws Exception {
+        int x = 10;
+        int y = 20;
+        if(x > y){
+            System.out.println("变量x大于变量y");
+        }
+        if(x < y){
+            System.out.println("变量x小于变量y");
+        }
+    }
+}
