e80189e43f071e252f81a73d5e90feeccfe2ad46
rtrrsr <376477035@qq.com>
2020-08-30 11:21:40 +0800

    利用for循环打印菱形

---
+++ ProcessControl/rhombus.java
@@ -0,0 +1,31 @@
+/**
+ * 利用for循环打印菱形
+ */
+package ProcessControl;
+import java.util.Scanner;
+public class rhombus {
+    public static void main(String[] args) throws Exception {
+        int num;
+        System.out.println("请输入一个大于1的奇数");
+        Scanner input = new Scanner(System.in);
+        num = input.nextInt();
+        for (int i = 1; i <= (num+1)/2; i++) {
+            for (int j = 1; j <= (num+1)/2-i; j++) {
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= 2*i-1; k++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+        for (int i = 1; i <= (num-1)/2; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = num-2; k >= 2*i-1; k--) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
