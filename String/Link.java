7f2cf101f5330271037308669bbde2d1a76f2590
rtrrsr <376477035@qq.com>
2020-08-31 14:57:47 +0800

    连接其他数据类型

---
+++ String/Link.java
@@ -0,0 +1,10 @@
+package String;
+//连接其他数据类型
+public class Link {
+    //主方法 权限修饰符 静态修饰符 返回值修饰符
+    public static void main(String[] args) {
+        int booktime = 4;
+        float practice = 2.5f;
+        System.out.println("我每天花费" + booktime + "小时看书，" + practice + "小时上机。");
+    }
+}
