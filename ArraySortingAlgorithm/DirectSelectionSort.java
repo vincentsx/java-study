6995b2a8dd5c9a0cc1780e01c0bfd3bd80e87ea4
rtrrsr <376477035@qq.com>
2020-09-02 20:47:23 +0800

    直接选择排序

--- ArraySortingAlgorithm/DirectSelectionSort.java
+++ ArraySortingAlgorithm/DirectSelectionSort.java
@@ -25,5 +25,43 @@ public class DirectSelectionSort {
             System.out.print(arr[i] + " ");
         }
         System.out.print("\n");     //换行
+        //创建直接排序类的对象
+        DirectSelectionSort sorter = new DirectSelectionSort();
+        //调用排序对象的方法将数组排序
+        sorter.directly(arr);
+    }
+    /**
+     * 直接选择排序
+     * @param arr
+     * 要排序的数组
+     */
+    private void directly(int[] arr) {
+        int index = 0;
+        for (int i = 1; i < arr.length; i++) {
+            index = 0;
+            for (int j = 0; j <= arr.length-i; j++) {
+                //假设arr[index]最小
+                if(arr[index] < arr[j] ) {
+                    index = j;
+                }
+            }
+            //元素交换
+            int temp = arr[arr.length-i];       //依次把末尾元素保存到临时变量值
+            arr[arr.length-i] = arr[index];     //依次将最大的元素保存到末尾元素
+            arr[index] = temp;                  //依次将末尾元素保存到最大的元素位置
+        }
+        showArray(arr);                         //输出排序后的数组元素
+    }
+    /**
+     * 显示数组中的所有元素
+     * @param arr
+     * 要显示的元素
+     */
+    private void showArray(int[] arr) {
+        System.out.print("排序后的数组:");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.print("\n");                 //换行
     }
 }
