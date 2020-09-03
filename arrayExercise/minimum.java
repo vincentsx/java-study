792aca4fb2fcf0beff6bbbfbc4b4ddd70db3eafb
rtrrsr <376477035@qq.com>
2020-09-03 16:50:16 +0800

    将数组中最小的数输出

---
+++ arrayExercise/minimum.java
@@ -0,0 +1,14 @@
+/**
+ * 将数组中最小的数输出
+ */
+
+package arrayExercise;
+//导入类
+import java.util.Arrays;
+//创建类
+public class minimum {
+    //主方法 权限修饰符 静态修饰符 返回值修饰符
+    public static void main(String[] args) {
+        //
+    }
+}
