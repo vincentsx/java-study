a95317a614553c75ce9cda1672e4474c2a647958
rtrrsr <376477035@qq.com>
2020-09-03 18:02:44 +0800

    将数组中最小的数输出

--- arrayExercise/minimum.java
+++ arrayExercise/minimum.java
@@ -11,6 +11,7 @@ public class minimum {
     //主方法 权限修饰符 静态修饰符 返回值修饰符
     public static void main(String[] args) {
         //输入数组
+        System.out.print("请输入一个数组(数组元素以空格间隔):");
         Scanner str = new Scanner(System.in);
         String[] arrs = null;
         arrs = str.nextLine().split(" ");
@@ -18,14 +19,24 @@ public class minimum {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = Integer.valueOf(arrs[i]);
         }
-        //显示原数组
+        //输出原数组
+        System.out.print("原数组是:");
+        showArray(arr);
+        //按从小到大排序
+        Arrays.sort(arr);
+        //输出最小的元素
+        System.out.println("数组arr中最小的元素是:" + arr[0]);
     }
+    
     /**
      * 显示数组中的元素
-     * @param arr
-     * 要显示的数组
+     * 
+     * @param arrs 要显示的数组
      */
-    public static void showArray(String arr) {
-        //
+    public static void showArray(int[] arrs) {
+        for (int i : arrs) {
+            System.out.print(i + " ");
+        }
+        System.out.print("\n");
     }
 }
